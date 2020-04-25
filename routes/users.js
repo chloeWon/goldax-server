96596bfc84c804fe0da958b5b593291411497c72
chloewon <wonsyung@gmail.com>
2020-04-26 02:54:06 +0900

    회원가입 코드 추가

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,46 @@
 const express = require('express');
+const mysql = require('mysql2/promise');
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const pool = mysql.createPool({
+  host: 'goldax.cfzgksdabvfh.ap-northeast-2.rds.amazonaws.com',
+  user: 'goldax',
+  database: 'example',
+  password: 'awsdkrak!',
+  connectionLimit : 10
 });
+
+
+router.post('/join', async (req, res, next) => {
+  const email = req.body.email;
+  const pw = req.body.pw;
+  const nickname = req.body.nickname;
+  const studentNum = req.body.studentNum;
+
+  const NICKNAME_CHECK_QUERY = 'SELECT nickname FROM Users WHERE nickname = ?';
+  const EMAIL_CHECK_QUERY = 'SELECT email FROM Users WHERE email = ?';
+  const USER_JOIN_QUERY = 'INSERT INTO Users (email, pw, nickname, studentNum) VALUES (?, ?, ?, ?)';
+
+  try {
+    const connection = await pool.getConnection(async (conn) => conn);
+    const [nicknames] = await connection.query(NICKNAME_CHECK_QUERY, nickname);
+
+    if (nicknames.length >= 1) {
+      return res.status(400).json({message: "DUPLICATED_NICKNAME"})
+    }
+
+    const [emails] = await connection.query(EMAIL_CHECK_QUERY, email);
+    if (emails.length >= 1) {
+      return res.status(400).json({message: "DUPLICATED_EMAIL"})
+    }
+
+    await connection.query(USER_JOIN_QUERY, [email, pw, nickname, studentNum]);
+
+  } catch (e) {
+    console.log(e)
+    return res.status(500).json({message: "JOIN ERROR"})
+  }
+  return res.status(200).json({message: "JOIN SUCCESS"});
+})
 
 module.exports = router;
